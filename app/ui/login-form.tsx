1fc82b26b7235d4a6dafc519890587b3e825d0aa
RaychelleHe <zounanhe@163.com>
2024-04-12 01:20:32 +0800

    remove php add axios

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -60,6 +60,30 @@ export default function LoginForm() {
               <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
+          <div className="mt-4">
+            <label
+              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+              htmlFor="identity"
+            >
+              identity
+            </label>
+            <div className="relative">
+              <input
+                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                id="identity"
+                type="radio"
+                name="identity"
+                required
+              />
+              <input
+                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                id="identity"
+                type="radio"
+                name="identity"
+                required
+              />
+            </div>
+          </div>
         </div>
         <LoginButton />
         <div
